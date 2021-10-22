dcc4d6ddb7f91067d06fcbc47c7e81d643ebdf8f
Redoy Kumar Shaha <83264166+RedoyRKS@users.noreply.github.com>
2021-10-22 22:49:49 +0600

    Create Crow.java

---
+++ Abstraction_and_Interface/Crow.java
@@ -0,0 +1,17 @@
+public class Crow implements Animal{
+    @Override
+    public void animalSound(){
+        System.out.println("ka ka ka!!!");
+
+    }
+
+    @Override
+    public void fly(){
+        System.out.println("fly Crow fly!!");
+
+    }
+    public void drinking(){
+        System.out.println("Crow drinking Water");
+    }
+    
+}
