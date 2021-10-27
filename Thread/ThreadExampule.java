fc386d7dded3e552078991c5de149b20fa8781b8
Redoy Kumar Saha <83264166+RedoyRKS@users.noreply.github.com>
2021-10-27 22:38:04 +0600

    Create ThreadExampule.java

---
+++ Thread/ThreadExampule.java
@@ -0,0 +1,15 @@
+public class ThreadExampule extends Thread {
+    @Override
+   public void run(){
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("Thread " + Thread.currentThread().getId() + " is running & value is " + i);
+            try{
+                Thread.sleep(100);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
